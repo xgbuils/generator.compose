d5afe7b8f6beb7f2a925f2240f7ef87bfb4c6ce3
Xavier Garcia Buils <xgbuils@gmail.com>
2017-03-22 22:09:49 +0100

    reimplemnt compose function following readme introduction

--- test/index_test.js
+++ test/index_test.js
@@ -2,107 +2,112 @@ const {expect} = require('chai')
 const compose = require('../src/')
 
 describe('compose', function () {
-    describe('compose range generators creating a new generator that', function () {
-        it('returns array of values [i, j, k] where i <= j <= k', function () {
-            const generator = compose(
-                function* (n, _) {
-                    _(_)
-                    yield* range(0, n)
-                },
-                function* (i, _) {
-                    _(i, _)
-                    yield* range(0, i)
-                },
-                function* (i, j, _) {
-                    _(i, j, _)
-                    yield* range(0, j)
-                },
-                function* (i, j, k) {
-                    yield [k, j, i]
+    describe('with simple generators', function () {
+        it('compose(add1, square)', function () {
+            const gen = compose(add1, square)
+            const iterator = gen(3)
+            expect(iterator.next()).to.be.deep.equal({
+                value: 10,
+                done: false
+            })
+        })
+
+        it('compose(square, add1)', function () {
+            const gen = compose(square, add1)
+            const iterator = gen(3)
+            expect(iterator.next()).to.be.deep.equal({
+                value: 16,
+                done: false
+            })
+        })
+    })
+    describe('using compose for repetition', function () {
+        it('compose(range.bind(null, 3), range)', function () {
+            const gen = compose(range.bind(null, 3), range)
+            expect([...gen(2)]).to.be.deep.equal([0, 1, 2, 0, 1, 2])
+        })
+
+        it('compose(range.bind(null, 2), range)', function () {
+            const gen = compose(range.bind(null, 2), range)
+            expect([...gen(5)]).to.be.deep.equal([0, 1, 0, 1, 0, 1, 0, 1, 0, 1])
+        })
+    })
+    describe('triangular replication', function () {
+        it('compose(range, range)', function () {
+            const gen = compose(range, range)
+            expect([...gen(5)]).to.be.deep.equal([0, 0, 1, 0, 1, 2, 0, 1, 2, 3])
+        })
+    })
+
+    describe('cartesian product', function () {
+        let start
+        let middle
+        let end
+        beforeEach(function () {
+            start = function* (n) {
+                for (let i = 1; i <= n; ++i) {
+                    yield {
+                        n,
+                        arr: [i]
+                    }
                 }
-            )
-            expect([...generator(2)]).to.be.deep.equal([
-                [0, 0, 0],
-                [0, 0, 1],
-                [0, 1, 1],
+            }
+            middle = function* (obj) {
+                const {n, arr} = obj
+                for (let i = 1; i <= n; ++i) {
+                    yield {
+                        n,
+                        arr: [...arr, i]
+                    }
+                }
+            }
+            end = function* (obj) {
+                yield obj.arr
+            }
+        })
+        it('[1, 2, 3] x [1, 2, 3]', function () {
+            const generators = [end, middle, start]
+            const gen = compose(...generators)
+            expect([...gen(3)]).to.be.deep.equal([
+                [1, 1],
+                [1, 2],
+                [1, 3],
+                [2, 1],
+                [2, 2],
+                [2, 3],
+                [3, 1],
+                [3, 2],
+                [3, 3]
+            ])
+        })
+
+        it('[1, 2] x [1, 2] x [1, 2]', function () {
+            const generators = [end, middle, middle, start]
+            const gen = compose(...generators)
+            expect([...gen(2)]).to.be.deep.equal([
                 [1, 1, 1],
-                [0, 0, 2],
-                [0, 1, 2],
                 [1, 1, 2],
-                [0, 2, 2],
+                [1, 2, 1],
                 [1, 2, 2],
+                [2, 1, 1],
+                [2, 1, 2],
+                [2, 2, 1],
                 [2, 2, 2]
             ])
-        })
-
-        it('returns values following this sequence: (0, 2, 4, 6, 100) x 3', function () {
-            const generator = compose(
-                function* (_) {
-                    _(_)
-                    yield* range(1, 6)
-                },
-                function* (i) {
-                    if (i % 2 === 1) {
-                        yield* range(0, 6, 2)
-                    } else {
-                        yield 100
-                    }
-                }
-            )
-            expect([...generator()]).to.be.deep.equal([
-                0, 2, 4, 6, 100, 0, 2, 4, 6, 100, 0, 2, 4, 6, 100
-            ])
-        })
-
-        it('if it is called the same generator twice, then returns the same result', function () {
-            const generator = compose(
-                function* (_) {
-                    _(_)
-                    yield* range(1, 6)
-                },
-                function* (i) {
-                    if (i % 2 === 1) {
-                        yield* range(0, 6, 2)
-                    } else {
-                        yield 100
-                    }
-                }
-            )
-            expect([...generator()])
-                .to.be.deep.equal([...generator()])
-        })
-    })
-
-    describe('test using empty generators', function () {
-        it('does not return values when parent generator is empty', function () {
-            const generator = compose(
-                function* (_) {
-                    _(_)
-                    yield* range(1, 3)[Symbol.iterator]()
-                },
-                function* (i) {
-                    yield* i % 2 === 1 ? [1] : []
-                },
-                function* () {
-                    yield* [1, 2, 3]
-                }
-            )
-            expect([...generator()]).to.be.deep.equal([
-                1, 2, 3, 1, 2, 3
-            ])
-        })
-    })
-
-    describe('when 0 arguments are passed', function () {
-        it('empty generator is created', function () {
-            const generator = compose()
-            expect([...generator()]).to.be.deep.equal([])
         })
     })
 })
 
-function* range (a, b, inc = 1) {
-    for (let i = a; i <= b; i += inc) {
+function* add1 (x) {
+    yield x + 1
+}
+
+function* square (x) {
+    yield x * x
+}
+
+function* range (n) {
+    for (let i = 0; i < n; ++i) {
         yield i
     }
 }
